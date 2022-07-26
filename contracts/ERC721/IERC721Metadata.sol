3d5736c12e951296c4562c66e00749bbe6b998a5
jihyun kim <jihyunivy@gmail.com>
2022-07-26 17:49:48 +0900

    [IERC721Metadata]

---
+++ contracts/ERC721/IERC721Metadata.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+interface IERC721Metadata {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function tokenURI(uint tokenId) external view returns (string memory);
+}
